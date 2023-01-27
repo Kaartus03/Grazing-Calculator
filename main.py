c1b7349b8676a6032c035978a2f11e6ac25a9a96
Kaartus03 <120675330+Kaartus03@users.noreply.github.com>
2023-01-27 13:17:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ st.text(
         " W= Percentage of Body Weight consumption of animal (kg)."
         " A = Total Area Required for grazing (Acres) "
         "C = Animal Units in Nos. (1 Unit = 720 kg ≈ 1 Cow or 7 Sheeps) "
-        "G = Grass yield (Dry Feed in kg/acres)")
+        "G = Grass yield per day (Dry Feed in kg/acres)")
 
 # Input Values
 weight = st.number_input("Enter Percentage Body Weight of Animal Unit in KG", step=1)
 C = st.number_input("Enter the Total Number of Animal Units in No.", step=1)
-G = st.number_input("Enter the Grass Yield per Day (kg/d)", step=0.1)
+G = st.number_input("Enter the Grass Yield per Day (kg/acres)", step=0.1)
 Area = (weight*C)/(0.36*G)
 
 st.success(f"Total area Required is {Area}")
